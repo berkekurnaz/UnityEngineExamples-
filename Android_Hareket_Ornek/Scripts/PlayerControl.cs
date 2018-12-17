b42fda8b6ec45a74c9bf3b4c1884eb653dbd50da
BerkeKurnaz <34648016+berkekurnaz@users.noreply.github.com>
2018-12-17 12:54:52 +0300

    Add files via upload

---
+++ Android_Hareket_Ornek/Scripts/PlayerControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    float dirX;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 700f;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal");
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            doJump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+    }
+
+    public void doJump()
+    {
+        if (rb.velocity.y == 0)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+        }
+    }
+
+}
