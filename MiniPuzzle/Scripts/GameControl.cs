2a25e992f5c2b515fdf83a4164db3d9c84062e0a
BerkeKurnaz <34648016+berkekurnaz@users.noreply.github.com>
2019-01-23 16:27:27 +0300

    Add files via upload

---
+++ MiniPuzzle/Scripts/GameControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform[] pictures;
+
+    [SerializeField]
+    private GameObject CanvasWin;
+
+    public static bool youWin;
+
+    void Start()
+    {
+        CanvasWin.SetActive(false);
+        youWin = false;
+    }
+
+    void Update()
+    {
+        if(pictures[0].rotation.z == 0 &&
+           pictures[1].rotation.z == 0 &&
+           pictures[2].rotation.z == 0 &&
+           pictures[3].rotation.z == 0 &&
+           pictures[4].rotation.z == 0 &&
+           pictures[5].rotation.z == 0 &&
+           pictures[6].rotation.z == 0 &&
+           pictures[7].rotation.z == 0)
+        {
+            youWin = true;
+            CanvasWin.SetActive(true);
+        }
+
+    }
+}
---
+++ MiniPuzzle/Scripts/TouchRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        if(!GameControl.youWin)
+        {
+            transform.Rotate(0f, 0f, 90f);
+        }
+    }
+
+}
